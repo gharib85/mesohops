f9f48af41cd90b3bec99bbe58c6ad64045302fec
Leonel-V <55210719+Leonel-V@users.noreply.github.com>
2020-08-14 16:24:19 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,8 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./mesohops'))
 #sys.path.insert(0, os.path.abspath('/Users/leovarvelo/Documents/PythonLibrary/pyhopslib/pyhops'))
 #sys.path.insert(0, os.path.abspath('/Users/leovarvelo/Documents/Sphinx/copy_pyhops/pyhops'))
 
