8d77eb418059a41177923a2d6f9145d8c2f32998
Leonel-V <55210719+Leonel-V@users.noreply.github.com>
2020-08-15 11:24:33 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rdt_theme'
+#html_theme = 'sphinx_rdt_theme'
+import guzzle_sphinx_theme
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "MESOHOPS",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
