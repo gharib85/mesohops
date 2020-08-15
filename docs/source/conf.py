fb0a3dbb4d0631e3037b1fea049a9dd973517da2
Leonel-V <55210719+Leonel-V@users.noreply.github.com>
2020-08-15 11:36:27 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark','sphinx.ext.autodoc','numpydoc'
+import sphinx_rdt_theme
+extensions = ['recommonmark','sphinx.ext.autodoc','numpydoc',"sphinx_rtd_theme"
 ]
 #napoleon_google_docstring = False
 # Add any paths that contain templates here, relative to this directory.
@@ -48,20 +49,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_rdt_theme'
-import guzzle_sphinx_theme
+html_theme = 'sphinx_rdt_theme'
 
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
-
-# Register the theme as an extension to generate a sitemap.xml
-extensions.append("guzzle_sphinx_theme")
-
-# Guzzle theme options (see theme.conf for more information)
-html_theme_options = {
-    # Set the name of the project to appear in the sidebar
-    "project_nav_name": "MESOHOPS",
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
